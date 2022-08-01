720626a5a4c691379f75772a1a43b1a79201a25d
Aleh Hatalski <aleh.hatalski@wolt.com>
2022-08-01 16:55:25 +0200

    New action check run

--- MathUtils/src/test/java/org/test/math/utils/FibonacciUtilTest.java
+++ MathUtils/src/test/java/org/test/math/utils/FibonacciUtilTest.java
@@ -11,19 +11,19 @@ public class FibonacciUtilTest {
         test(fibonacciUtil);
     }
 
-    @Test
-    public void FibonacciParallelTest()
-    {
-        FibonacciUtil fibonacciUtil= new FibonacciLinearUtil();
-        test(fibonacciUtil);
-    }
-
-    @Test
-    public void FibonacciRecurTest()
-    {
-        FibonacciUtil fibonacciUtil= new FibonacciRecurUtil();
-        test(fibonacciUtil);
-    }
+//    @Test
+//    public void FibonacciParallelTest()
+//    {
+//        FibonacciUtil fibonacciUtil= new FibonacciLinearUtil();
+//        test(fibonacciUtil);
+//    }
+//
+//    @Test
+//    public void FibonacciRecurTest()
+//    {
+//        FibonacciUtil fibonacciUtil= new FibonacciRecurUtil();
+//        test(fibonacciUtil);
+//    }
 
     @Test
     public void FibonacciLinearTest()
@@ -36,7 +36,7 @@ public class FibonacciUtilTest {
     private void test(FibonacciUtil fibonacciUtil) {
         Assert.assertEquals(fibonacciUtil.calc(1L),1L);
         Assert.assertEquals(fibonacciUtil.calc(2L),1L);
-        Assert.assertEquals(fibonacciUtil.calc(3L),2L);
+        Assert.assertEquals(fibonacciUtil.calc(3L),5L);
         Assert.assertEquals(fibonacciUtil.calc(10L),55L);
         Assert.assertEquals(fibonacciUtil.calc(100L),3736710778780434371L);
     }
